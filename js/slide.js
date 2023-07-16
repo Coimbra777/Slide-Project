97c2a3993b9d5f987420a3689929d426a5d86b4e
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2023-07-16 12:38:41 -0300

    primeiro commimt

---
+++ js/slide.js
@@ -0,0 +1,40 @@
+export default class Slide {
+  constructor(slide, wrapper) {
+    this.slide = document.querySelector(slide);
+    this.wrapper = document.querySelector(wrapper);
+  }
+
+  onStart(event) {
+    event.preventDefault();
+    // console.log("mousedown");
+    this.wrapper.addEventListener("mousemove", this.onMove);
+  }
+
+  onMove() {
+    // console.log("moveu");
+  }
+
+  onEnd() {
+    // console.log("acabou");
+    this.wrapper.removeEventListener("mousemove", this.onMove);
+  }
+
+  // ADICIONA EVENTOS AO WRAPPER
+  addSlideEvents() {
+    this.wrapper.addEventListener("mousedown", this.onStart);
+    this.wrapper.addEventListener("mouseup", this.onEnd);
+  }
+
+  // metodo para referenciar o this a class Slide
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+  }
+
+  init() {
+    this.bindEvents();
+    this.addSlideEvents();
+    return this;
+  }
+}
